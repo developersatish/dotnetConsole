e116295a07413a506971e886a67c168a09397e79
Satish Singh <satishsingh.net@outlook.com>
2021-01-30 17:32:31 +0530

    init

---
+++ DotnetConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
